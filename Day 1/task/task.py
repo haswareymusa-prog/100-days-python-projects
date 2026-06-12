ccf117ee5ac1b8829f461021c3256972768b3556
Musa Haswarey <haswareymusa@gmail.com>
2026-06-11 21:17:33 -0500

    Add Day 1 band name generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+name1 = input("What's the name of the city you grew up in?")
+
+name2 = input("What's your pet's name?")
+
+print("Your band name could be " + name1 + " " + name2)
